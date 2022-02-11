3a388cf6d3cfa69c2fc038ab015051c18e230a7a
sanka <sanka@LAPTOP-UR1U4R4C.lan>
2022-02-11 14:12:34 -0500

    Added Description about this file

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -9,6 +9,12 @@ import com.acti.pages.EnterTimePage;
 import com.acti.pages.LoginPage;
 import com.acti.utils.ExcelLib;
 
+/*
+ * Name: BaseTest
+ * Description: This Script is a paretn script which has tthe set of prerequisite stepa before executing actual test case
+ * Author:Shantosh
+ * Date Modified: 2/11/2022
+ */
 public class BaseTest extends DriverScript {
 
 	LoginPage lp;
